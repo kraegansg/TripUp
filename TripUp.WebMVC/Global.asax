fc7f0aac81c0f8acefad8cd1001fd1da79388aa2
ksgraves <kraegan.s.g@gmail.com>
2021-07-25 10:34:15 -0400

    Add project files.

---
+++ TripUp.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TripUp.WebMVC.MvcApplication" Language="C#" %>
---
+++ TripUp.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TripUp.WebMVC.Startup))]
+namespace TripUp.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
